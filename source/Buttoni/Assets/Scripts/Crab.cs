1ef146e696db8d723720ec56d86a3d97ac4926f7
TheCell <simon_hischier@hotmail.com>
2019-01-26 17:29:04 +0100

    some joint tests for the crab

---
+++ source/Buttoni/Assets/Scripts/Crab.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab : MonoBehaviour
+{
+	private GameObject crab;
+	private float legForce = 300.0f;
+	public GameObject leg1;
+	public GameObject leg2;
+	public GameObject leg3;
+	public GameObject leg4;
+	public GameObject leg5;
+	public GameObject leg6;
+	public GameObject leg7;
+	public GameObject leg8;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		crab = gameObject;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		moveLeg();
+	}
+
+	private void moveLeg()
+	{
+
+		if (Input.GetButton("8"))
+		{
+		}
+		else
+		{
+		}
+		
+		if (Input.GetButton("i"))
+		{
+			addForce(leg6);
+			addForce(leg8);
+		}
+		else
+		{
+			HingeJoint hinge = leg6.GetComponent<HingeJoint>();
+			hinge.useMotor = false;
+			HingeJoint hinge8 = leg8.GetComponent<HingeJoint>();
+			hinge8.useMotor = false;
+		}
+		if (Input.GetButton("k"))
+		{
+			addForce(leg5);
+			addForce(leg7);
+		}
+		else
+		{
+			HingeJoint hinge5 = leg5.GetComponent<HingeJoint>();
+			hinge5.useMotor = false;
+			HingeJoint hinge = leg7.GetComponent<HingeJoint>();
+			hinge.useMotor = false;
+		}
+		if (Input.GetButton("m"))
+		{
+		}
+		else
+		{
+		}
+
+
+		if (Input.GetButton("3"))
+		{
+		}
+		else
+		{
+		}
+		if (Input.GetButton("w"))
+		{
+			addForce(leg2);
+			addForce(leg4);
+		}
+		else
+		{
+			HingeJoint hinge = leg2.GetComponent<HingeJoint>();
+			hinge.useMotor = false;
+			HingeJoint hinge4 = leg4.GetComponent<HingeJoint>();
+			hinge4.useMotor = false;
+		}
+		if (Input.GetButton("s"))
+		{
+			addForce(leg1);
+			addForce(leg3);
+		}
+		else
+		{
+			HingeJoint hinge1 = leg1.GetComponent<HingeJoint>();
+			hinge1.useMotor = false;
+			HingeJoint hinge = leg3.GetComponent<HingeJoint>();
+			hinge.useMotor = false;
+		}
+		if (Input.GetButton("x"))
+		{
+		}
+		else
+		{
+		}
+	}
+
+	private void addForce(GameObject leg)
+	{
+		HingeJoint hinge = leg.GetComponent<HingeJoint>();
+
+		// Make the hinge motor rotate with 90 degrees per second and a strong force.
+		JointMotor motor = hinge.motor;
+		motor.force = legForce;
+		motor.targetVelocity = -90;
+		motor.freeSpin = false;
+		hinge.motor = motor;
+		hinge.useMotor = true;
+	}
+}
---
+++ source/Buttoni/Assets/Scripts/JointTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointTest : MonoBehaviour
+{
+	public HingeJoint legpartHinge1;
+	public HingeJoint legpartHinge5;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButton("8"))
+		{
+			closeJoint(legpartHinge1);
+		}
+		else
+		{
+			openJoint(legpartHinge1);
+		}
+
+
+		if (Input.GetButton("3"))
+		{
+			closeJoint(legpartHinge5);
+		}
+		else
+		{
+			openJoint(legpartHinge5);
+		}
+	}
+
+	void closeJoint(HingeJoint joint)
+	{
+		JointSpring jointSpring = joint.spring;
+		jointSpring.targetPosition = -160.0f;
+		joint.spring = jointSpring;
+	}
+
+	void openJoint(HingeJoint joint)
+	{
+		JointSpring jointSpring = joint.spring;
+		jointSpring.targetPosition = -60.0f;
+		joint.spring = jointSpring;
+	}
+}
