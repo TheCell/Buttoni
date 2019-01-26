a44bff21e877a012efa0e69145b7ebe6a3d5bf47
Fl0ps <florian.etgeton@gmail.com>
2019-01-26 18:21:32 +0100

    Snail Scene modified
    added fence and rocks
    camera follows palyer

---
+++ source/Buttoni/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + pos;
+    }
+}
