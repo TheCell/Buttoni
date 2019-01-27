912f3def3c13907f45d87eee9afbb885820661ee
TheCell <simon_hischier@hotmail.com>
2019-01-27 07:50:51 +0100

    added bird and misc to millipede

---
+++ source/Buttoni/Assets/Scripts/RotateBird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBird : MonoBehaviour
+{
+	private float currentRotation;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		currentRotation = 0.0f;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void FixedUpdate()
+	{
+		currentRotation += 0.5f;
+		Quaternion rotateTo = Quaternion.Euler(-90.0f, currentRotation, 0.0f);
+		gameObject.transform.localRotation = rotateTo;
+	}
+}
