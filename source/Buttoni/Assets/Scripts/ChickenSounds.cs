9175e4dc0a865298907ba7b5bd60b22acbc98326
TheCell <simon_hischier@hotmail.com>
2019-01-27 15:28:00 +0100

    chicken sounds

---
+++ source/Buttoni/Assets/Scripts/ChickenSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenSounds : MonoBehaviour
+{
+	private AudioSource audioSource;
+	public AudioClip[] audioclips;
+	private float minDelay = 6.0f;
+	private float lastSound;
+	private float timeTillNextSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		lastSound = Time.realtimeSinceStartup;
+		audioSource = gameObject.GetComponent<AudioSource>();
+		timeTillNextSound = Random.value * 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastSound + minDelay + timeTillNextSound < Time.realtimeSinceStartup)
+		{
+			timeTillNextSound = Random.value * 10.0f;
+			lastSound = Time.realtimeSinceStartup;
+			playSound();
+		}
+    }
+
+	void playSound()
+	{
+		audioSource.PlayOneShot(audioclips[(int)Mathf.Floor(Random.value * audioclips.Length)]);
+	}
+}
