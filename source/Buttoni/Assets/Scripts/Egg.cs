a41426cd7f4aa1941059c01cf6aeb490d7e4f06e
TheCell <simon_hischier@hotmail.com>
2019-01-31 11:38:43 +0100

    changed egg forces

--- source/Buttoni/Assets/Scripts/Egg.cs
+++ source/Buttoni/Assets/Scripts/Egg.cs
@@ -5,51 +5,78 @@ using UnityEngine;
 public class Egg : MonoBehaviour
 {
 	private GameObject egg;
-	private float pushForce = 1000.0f;
+	private float pushForce = 10.0f;
+    private float upForce = 10.0f;
 	private AudioSource audioSource;
 	public AudioClip[] audioClips;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
 		egg = gameObject;
+        rb = egg.GetComponent<Rigidbody>();
 		audioSource = egg.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
 	{
-		if (Input.GetButtonDown("z"))
-		{
-			pushLeft();
-		}
+    }
 
-		if (Input.GetButtonDown("t"))
-		{
-			pushRight();
-		}
-	}
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("z"))
+        {
+            pushLeft();
+        }
 
-	void OnCollisionEnter(Collision collision)
+        if (Input.GetButton("t"))
+        {
+            pushRight();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
 	{
 		audioSource.PlayOneShot(audioClips[(int)Mathf.Floor(Random.value * audioClips.Length)]);
 	}
 
 	private void pushLeft()
 	{
-		Vector3 force = new Vector3(pushForce * Time.deltaTime, pushForce * Time.deltaTime, 0.0f);
-		addForce(force);
-	}
+        Vector3 force;
+
+        if (Mathf.Abs(rb.velocity.y) < upForce)
+        {
+            force = new Vector3(pushForce, upForce, 0.0f);
+        }
+        else
+        {
+            force = new Vector3(pushForce, 0.0f, 0.0f);
+        }
+
+        if (rb.velocity.x < 5.0f)
+        {
+            rb.AddForce(force);
+        }
+    }
 
 	private void pushRight()
 	{
-		Vector3 force = new Vector3(-1 * (pushForce * Time.deltaTime), pushForce * Time.deltaTime, 0.0f);
-		addForce(force);
-	}
+        Vector3 force;
 
-	private void addForce(Vector3 force)
-	{
-		Rigidbody rb = egg.GetComponent<Rigidbody>();
-		rb.AddForce(force);
-	}
+        if (Mathf.Abs(rb.velocity.y) < upForce)
+        {
+            force = new Vector3(-1 * (pushForce), upForce, 0.0f);
+        }
+        else
+        {
+            force = new Vector3(-1 * (pushForce), 0.0f, 0.0f);
+        }
+
+        if (rb.velocity.x > -5.0f)
+        {
+            rb.AddForce(force);
+        }
+    }
 }
